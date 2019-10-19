e72707259b45cbb11b92a0e71cfbc89e98aab9f6
XhmikosR <xhmikosr@gmail.com>
2019-10-19 21:25:13 +0300

    Lint assets too.

--- assets/iframe.js
+++ assets/iframe.js
@@ -1,7 +1,6 @@
-/* global io,data */
+/* globals io:false, data:false */
 
-(function (){
-
+(function () {
   // give up and resort to `target=_blank`
   // if we're not modern enough
   if (!document.body.getBoundingClientRect
@@ -15,15 +14,15 @@
 
   // listen on setup event from the parent
   // to set up the id
-  window.addEventListener('message', function onmsg (e){
+  window.addEventListener('message', function onmsg(e) {
     if (/^slackin:/.test(e.data)) {
       id = e.data.replace(/^slackin:/, '')
-      document.body.addEventListener('click', function (ev){
+      document.body.addEventListener('click', function (ev) {
         var el = ev.target
-        while (el && 'A' != el.nodeName) el = el.parentNode
-        if (el && '_blank' === el.target) {
+        while (el && el.nodeName != 'A') el = el.parentNode
+        if (el && el.target === '_blank') {
           ev.preventDefault()
-          parent.postMessage('slackin-click:' + id, '*')
+          window.parent.postMessage('slackin-click:' + id, '*')
         }
       })
       window.removeEventListener('message', onmsg)
@@ -36,14 +35,14 @@
   // notify parent about current width
   var button = document.querySelector('.slack-button')
   var lastWidth
-  function refresh (){
+  function refresh() {
     var width = button.getBoundingClientRect().width
-    if (top != window && window.postMessage) {
+    if (window != window.top && window.postMessage) {
       var but = document.querySelector('.slack-button')
       var width = Math.ceil(but.getBoundingClientRect().width)
-      if (lastWidth != width) {
+      if (lastWidth !== width) {
         lastWidth = width
-        parent.postMessage('slackin-width:' + id + ':' + width, '*')
+        window.parent.postMessage('slackin-width:' + id + ':' + width, '*')
       }
     }
   }
@@ -51,23 +50,22 @@
   // initialize realtime events asynchronously
   var script = document.createElement('script')
   script.src = 'https://cdn.socket.io/socket.io-1.4.4.js'
-  script.onload = function (){
-
+  script.onload = function () {
     // use dom element for better cross browser compatibility
     var url = document.createElement('a')
     url.href = window.location
     var socket = io({ path: data.path + 'socket.io' })
     var count = document.getElementsByClassName('slack-count')[0]
 
-    socket.on('data', function (users){
+    socket.on('data', function (users) {
       for (var i in users) update(i, users[i])
     })
-    socket.on('total', function (n){ update('total', n) })
-    socket.on('active', function (n){ update('active', n) })
+    socket.on('total', function (n) { update('total', n) })
+    socket.on('active', function (n) { update('active', n) })
 
     var anim
-    function update (key, n) {
-      if (n != data[key]) {
+    function update(key, n) {
+      if (data[key] != n) {
         data[key] = n
         var str = ''
         if (data.active) str = data.active + '/'
@@ -76,7 +74,7 @@
         if (anim) clearTimeout(anim)
         count.innerHTML = str
         count.className = 'slack-count anim'
-        anim = setTimeout(function (){
+        anim = setTimeout(function () {
           count.className = 'slack-count'
         }, 200)
         refresh()
@@ -84,5 +82,4 @@
     }
   }
   document.body.appendChild(script)
-
-})()
+}())
